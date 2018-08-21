05ecca34cd60b1e4c58a6ae847c3a9fb71303d11
houyuxin <784929471@qq.com>
2018-08-21 23:24:31 +0800

    配置文件

---
+++ class/MYTool.h
@@ -0,0 +1,12 @@
+//
+//  MYTool.h
+//  
+//
+//  Created by hyx on 2018/8/21.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MYTool : NSObject
+
+@end
---
+++ class/MYTool.m
@@ -0,0 +1,12 @@
+//
+//  MYTool.m
+//  
+//
+//  Created by hyx on 2018/8/21.
+//
+
+#import "MYTool.h"
+
+@implementation MYTool
+
+@end
